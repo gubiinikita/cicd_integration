b8e093a708ecee651f80456632fc0c21ffb95c5d
clear <clear@MacBook-Pro-Nik.local>
2023-10-23 14:06:44 +0300

    updated config

---
+++ test/test.js
@@ -0,0 +1,25 @@
+// test.js
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('files', function () {
+  describe('export', function () {
+    it('should export pdf', async function () {
+      assert.isTrue(true);
+    });
+
+    it('should export html', async function () {
+      assert.isTrue(true);
+    });
+
+    it('should export yml', async function () {
+      assert.isTrue(true);
+    });
+
+    it('should export text', async function () {
+      throw new Error('An exception occurred');
+    });
+  });
+
+  // [..]
+});
