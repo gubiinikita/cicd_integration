6b8daabd694b66fe3a06aa8210fb49962e1cf6fc
clear <clear@MacBook-Pro-Nik.local>
2023-11-28 17:11:13 +0200

    check fails

--- test/test1.js
+++ test/test1.js
@@ -14,5 +14,9 @@ describe('files 1', function () {
     it('should export yml 1', async function () {
       assert.isTrue(true);
     });
+
+    it('should export text 1', async function () {
+      throw new Error('An exception occurred');
+    });
   });
 });
