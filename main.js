ec0422b8c95b06ea3e9c624bcd71004a0b7b1a8a
clear <clear@MacBook-Pro-Nik.local>
2023-12-14 15:08:02 +0200

    added rtportal

---
+++ main.js
@@ -0,0 +1,27 @@
+const Mocha = require('mocha');
+
+const mochaMain = new Mocha({
+  reporter: '@reportportal/agent-js-mocha',
+  reporterOptions: {
+    endpoint: 'https://rtportal.devopdata.co/api/v1',
+    apiKey: 'cccfcc8e-eb46-4121-9cf3-2f06d470196e',
+    launch: 'Test report',
+    project: 'mykyta_test_project',
+    description: 'Mykyta tries to build cicd pipline',
+  },
+  timeout: 250000,
+});
+
+// run tests
+try {
+  mochaMain.files = [
+    'spec/attributesTest.spec.js',
+    'spec/descriptionsTest.spec.js',
+    'spec/logTest.spec.js',
+    'spec/statusesTest.spec.js',
+    'spec/testCaseIdTest.spec.js',
+  ];
+  mochaMain.run((failures) => process.on('exit', () => process.exit(failures))); // exit with non-zero exit code, other wise fails will not fail mocha run
+} catch (err) {
+  console.error(`Test suite doesn't exists or set. Error: ${err}`);
+}
